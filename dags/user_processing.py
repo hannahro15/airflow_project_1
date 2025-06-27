e255dbe3bd14e4c4be2bcc437011a8d79e00f37c
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-06-27 11:13:11 +0100

    Add create table for a sql table in first dag function

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -1,7 +1,22 @@
 from airflow.sdk import dag
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 
 @dag
 def user_processing():
-    pass
+    
+    create_table = SQLExecuteQueryOperator(
+        task_id="create_table",
+        CONN_ID="postgres",
+        sql="""
+        CREATE TABLE IF NOT EXISTS user (
+        id INT PRIMARY KEY,
+        firstname VARCHAR(255),
+        lastname VARCHAR(255),
+        email VARCHAR(255),
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+        """
+
+    )
 
 user_processing()
