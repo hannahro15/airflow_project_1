f884dbe3495bef160018ccb1b746c9a5166884c5
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-06-30 11:04:12 +0100

    Retrieve api using the is_api_available function

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -1,5 +1,7 @@
-from airflow.sdk import dag
+from airflow.sdk import dag, task
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.sdk.bases.sensor import PokeReturnValue
+
 
 @dag
 def user_processing():
@@ -18,5 +20,20 @@ def user_processing():
         """
     
     )
+
+    @task.sensor(poke_interval=30, timeout=300)
+    def is_api_available() -> PokeReturnValue:
+        import requests
+        response = requests.get("https://superheroapi.com/api/id/biography")
+        print(response.status_code)
+        if response.status_code == 200:
+            condition = True
+            char_bio = response.json()
+        else:
+            condition = False
+            char_bio = None
+        return PokeReturnValue(is_done=condition, xcom_value=char_bio)
+
+    is_api_available()
 
 user_processing()
